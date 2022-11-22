b1de30f506f4ff898c0798d1170abbb14bc41f59
Gaurav Hungund <60032249+AsgardRodent@users.noreply.github.com>
2022-11-23 00:37:09 +0530

    Create Prime number.cpp

---
+++ Prime number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    if(a%2==1)
+    {
+        cout<<"It is a Prime Number";
+    }
+    else
+    {
+        cout<<"It is not a Prime Number";
+    }
+    return 0;
+}
