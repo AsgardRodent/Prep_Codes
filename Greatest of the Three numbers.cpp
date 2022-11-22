9badba0d4ba7232a1daf7e060a9ba55c84049017
Gaurav Hungund <60032249+AsgardRodent@users.noreply.github.com>
2022-11-23 00:31:14 +0530

    Create Greatest of the Three numbers.cpp

---
+++ Greatest of the Three numbers.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,temp;
+    cin>>a>>b>>c;
+    if(a > b && a > c)
+    {
+        temp = a;
+    }
+    else if (b > a && b > c) 
+    {
+        temp = b;
+    }
+    else 
+    {
+        temp = c;
+    }
+    cout<<"Greatest is: "<<temp;
+    return 0;
+}
